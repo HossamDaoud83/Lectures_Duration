74c601d4368605e7cf6b4fb18404d18b23973cfa
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-12-10 11:07:12 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="GSB", page_icon="16.png")
-
+st.set_page_config(page_title="Lectures Duration", page_icon="16.png")
 st.image("GSB Medium.png", width=120,)
-st.title("Microsoft Teams Lectures Duration")
-st.image("Power-Bi-Service.jpg", width=600,)
 
+st.title("Microsoft Teams Lectures Duration")
+components.iframe("https://app.powerbi.com/view?r=eyJrIjoiZGI2OTc3Y2ItM2JjYS00MzZmLWI5YjUtOTY4OWIwODdkYjhiIiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9", width=800, height=600)
 
 hide_st_style = """
             <style>
@@ -15,12 +14,3 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-
-# Create a button that links to an external website
-external_link = "https://laila-ai-gsb.streamlit.app/"  # Replace with the actual URL
-if st.button("Chat with Laila AI"):
-    st.image("Picture1.png", width=120,)       
-    st.markdown(f"Redirecting to [{external_link}]({external_link})")
-    # You can also use st.markdown to create a clickable link to the external website:
-    # st.markdown(f"[Visit External Website]({external_link})")
